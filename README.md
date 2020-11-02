b229864d24118c03b19d04c63ddcbaf3dc70db29
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-11-02 14:11:31 +0100

    dodanie hooków w App.js

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 Aplikacja stworzona na potrzeby zaliczenia projektu FreeCodeCamp \
 Obejrzyj na żywo: https://budnickip.github.io/drummachine/ \
 Wykorzystane technologie: \
-ReactJS, SCSS, Bootstrap, React Hooks(aktualnie w komponencie Button oraz Board) \
-Dodane technologie - aktualnie w projekcie zmieniane są komponenty klasowe na funkcyjne oraz wdrażane są React Hooki
+ReactJS, SCSS, Bootstrap, React Hooks(useState i useEffect - useEffect w komponencie Button) \
+Początkowo aplikacja była oparta na komponentach klasowych w celu zmiany state
+jednak w trakcie dalszej nauki poznałem moc React Hooks, dlatego zamieniłem komponenty klasowe na
+komponenty funkcyjne. Nie usuwałem komponentów stanowych, zostawiłem je w komentarzach, aby w każdej chwili móc przypomnieć
+sobie różnice pomiędzy tymi sposobami programowania
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.scss';
  import Board from './components/Board/Board'
 
-
+/*
 class App extends Component {
   constructor(props){
     super(props);
@@ -80,7 +80,70 @@ class App extends Component {
     );
   }
 }
+*/
 
+function App(){
+
+    const [isSoundButtons1, setIsSoundButtons1] = useState(true)
+    const [power, setPower] = useState(true)
+    const [volume, setVolume] = useState('0.4')
+    const [status, setStatus] = useState('')
+    const [isChecked, setIsChecked] = useState(true)
+
+    const handleChange = () => {
+      setIsSoundButtons1(!isSoundButtons1)
+    }
+  
+    const switchPower = () =>{
+      setPower(!power)
+    }
+  
+    const changeVolume = (e) => {
+      setVolume(e.target.value)
+      setStatus("Volume: " + Math.floor(volume * 100))
+
+      setTimeout(() => {
+        setStatus('')
+      }, 3000)
+    }
+
+    return (
+      <div id="drum-machine" className="container">
+        <div className="row machine">
+          <div className="col-md-6">
+            <Board isSoundButtons1 = {isSoundButtons1}  status = {status} power = {power} volume = {volume}/>       
+          </div>
+            <div className="col-md-6 text-center">
+              <div className="row">
+                <div className="power col-12">
+                  <p className="power__paragraph">Power:</p>
+                  <label className="switch">
+                    <input type="checkbox" checked = {power} onChange={switchPower}/>
+                    <span className="slider round"></span>
+                  </label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12">
+                   <input type="range" className="range" min="0" max="1" step='0.01' value={volume} volume = {volume} power = {power} onChange={changeVolume} />
+                </div>
+              </div>
+              <div className="row">
+                <div className="bank col-12">
+                  <p className="bank__paragraph">Bank:</p>
+                  <label className="switch">
+                    <input type="checkbox" checked = {isSoundButtons1} onChange={handleChange}/>
+                    <span className="slider round"></span>
+                  </label>
+                </div>
+              </div>
+
+            </div>
+        </div>
+      </div>
+    );
+
+}
 
 
 export default App;
